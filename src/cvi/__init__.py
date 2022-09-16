26132e571e53c58a5bb716675601b41f1e9e380b
Sasha Petrenko <sap625@umsystem.edu>
2022-09-16 10:12:09 -0500

    Bump version to v0.1.0-alpha.4

--- src/cvi/__init__.py
+++ src/cvi/__init__.py
@@ -2,7 +2,7 @@
 cvi - A Python library for both incremental and batch cluster validity indices.
 """
 
-__version__ = "0.1.0-alpha.3"
+__version__ = "0.1.0-alpha.4"
 
 from .modules import (
     CVI,
